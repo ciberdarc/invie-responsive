7f0e26d0bbbc6acf9cb6cb22b228566c9633cb8c
Alexis Araujo <ala1289@gmail.com>
2018-07-10 12:49:50 -0500

    Autenticacion google

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log(firebase)
+var btnLogin = document.getElementById("btnLogin");
+
+
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+// console.log(user);
+// if (user) {
+//     console.log("tenemos usuario");
+  
+// } else {
+//     console.log("no tenemos usuario");
+  
+// }
+});
+
+btnLogin.addEventListener("click", function name(params) {
+  event.preventDefault();
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  console.log("funciona btn");
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth().signInWithPopup(provider).then(function(datosusuario) {
+    console.log(datosusuario);
+    
+  }).catch(function(err) {
+    console.log("error");
+  });
+});
